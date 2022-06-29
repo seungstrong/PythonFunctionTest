279f7240ed578fca1ed8f46a2f5edc3f9a093560
Yeong Hun Lee <dudgns3781@gmail.com>
2022-06-29 11:26:26 +0900

    사칙연산 함수들

---
+++ Function.py
@@ -0,0 +1,13 @@
+#더하기
+def add(a,b):
+    return a+b
+#빼기
+def sub(a,b):
+    if a > b:
+        return a-b
+    else:
+        return b-a
+    
+#곱하기
+def mul(a,b):
+    return a*b
---
+++ README.md
@@ -0,0 +1,2 @@
+# 사칙연산 함수(나누기 제외)
+<img src = 'python.jpg' alt = "파이썬">
